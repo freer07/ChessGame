7167f7aeb90a095f73a8316f2cdb696005ffaf5c
Alexander Freer <chasealex6@hotmail.com>
2020-12-07 18:44:16 -0500

    Updated Piece.java

--- src/main/java/com/chessAI/Pieces/Piece.java
+++ src/main/java/com/chessAI/Pieces/Piece.java
@@ -2,8 +2,14 @@ package com.chessAI.Pieces;
 
 public class Piece {
     boolean white;
+    int row;
+    int column;
+    boolean isActive;
 
-    public Piece(boolean b) {
+    public Piece(boolean b, int x, int y) {
         white = b;
+        row = x;
+        column = y;
+        isActive = true;
     }
 }
