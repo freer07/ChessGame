52d827f8f63688e34fe48e03f4148dd2eada3a0e
Alexander Freer <chasealex6@hotmail.com>
2020-12-07 17:20:14 -0500

    added maven to the project

---
+++ src/main/java/com/chessAI/ChessAiApplication.java
@@ -0,0 +1,13 @@
+package com.chessAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chessAI/ChessAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
