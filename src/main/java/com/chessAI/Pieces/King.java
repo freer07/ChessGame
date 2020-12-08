26d9bc7b8549379193c9c33390fe8ccd87455aa4
Alexander Freer <chasealex6@hotmail.com>
2020-12-08 09:26:00 -0500

    added findAvailPos for Rook
    IMPORTANT: Rooks will not automatically add castling position to their list of possible positions because it must move both King and Rook, therefore there is a Flag within it

--- src/main/java/com/chessAI/Pieces/King.java
+++ src/main/java/com/chessAI/Pieces/King.java
@@ -3,6 +3,8 @@ package com.chessAI.Pieces;
 public class King extends Piece {
     private static final String abbrev = "K";
     private static final int value = 1000000;
+    public boolean hasMoved = false;
+    public boolean inCheck = false;
 
     public King(boolean b, int x, int y) {
         super(b, x, y, abbrev, value);
