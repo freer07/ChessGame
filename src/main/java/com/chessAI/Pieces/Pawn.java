ce677425a03d60bc9c488b179ec522019b20113c
Alexander Freer <chasealex6@hotmail.com>
2020-12-07 21:28:54 -0500

    added hasMoved to pawn

--- src/main/java/com/chessAI/Pieces/Pawn.java
+++ src/main/java/com/chessAI/Pieces/Pawn.java
@@ -3,6 +3,7 @@ package com.chessAI.Pieces;
 public class Pawn extends Piece {
     private static final String abbrev = "P";
     private static final int value = 10;
+    public boolean hasMoved = false;
 
     public Pawn(boolean b, int x, int y) {
         super(b, x, y, abbrev, value);
