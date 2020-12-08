df3efaa41665820357ad5244d357549c43b82035
Alexander Freer <chasealex6@hotmail.com>
2020-12-08 09:50:56 -0500

    added Find Avail Pos in king
    The King does NOT find the castling moves!

--- src/main/java/com/chessAI/Pieces/King.java
+++ src/main/java/com/chessAI/Pieces/King.java
@@ -9,4 +9,73 @@ public class King extends Piece {
     public King(boolean b, int x, int y) {
         super(b, x, y, abbrev, value);
     }
+
+    @Override
+    public void findAvailPos(Piece[][] boardLayout) {
+        super.findAvailPos(boardLayout);
+
+        //X+1 Y+1
+        if (X+1 < boardLayout.length && Y+1 < boardLayout.length) {
+            Piece pos = boardLayout[X+1][Y+1];
+            if (pos == null || pos.black != black) { //if empty or enemy then valid
+                addAvailPos(X+1, Y+1);
+            }
+        }
+
+        //X-1 Y-1
+        if (X-1 >= 0 && Y-1 >= 0) {
+            Piece pos = boardLayout[X-1][Y-1];
+            if (pos == null || pos.black != black) { //if empty or enemy then valid
+                addAvailPos(X-1, Y-1);
+            }
+        }
+
+        //X Y+1
+        if (Y+1 < boardLayout.length) {
+            Piece pos = boardLayout[X][Y+1];
+            if (pos == null || pos.black != black) { //if empty or enemy then valid
+                addAvailPos(X, Y+1);
+            }
+        }
+
+        //X+1 Y
+        if (X+1 < boardLayout.length) {
+            Piece pos = boardLayout[X+1][Y];
+            if (pos == null || pos.black != black) { //if empty or enemy then valid
+                addAvailPos(X+1, Y);
+            }
+        }
+
+        //X Y-1
+        if (Y-1 >= 0) {
+            Piece pos = boardLayout[X][Y-1];
+            if (pos == null || pos.black != black) { //if empty or enemy then valid
+                addAvailPos(X, Y-1);
+            }
+        }
+
+        //X-1 Y
+        if (X-1 >= 0) {
+            Piece pos = boardLayout[X-1][Y];
+            if (pos == null || pos.black != black) { //if empty or enemy then valid
+                addAvailPos(X-1, Y);
+            }
+        }
+
+        //X-1 Y+1
+        if (X-1 >= 0 && Y+1 < boardLayout.length) {
+            Piece pos = boardLayout[X-1][Y+1];
+            if (pos == null || pos.black != black) { //if empty or enemy then valid
+                addAvailPos(X-1, Y+1);
+            }
+        }
+
+        //X+1 Y-1
+        if (Y-1 >= 0 && X+1 < boardLayout.length) {
+            Piece pos = boardLayout[X+1][Y-1];
+            if (pos == null || pos.black != black) { //if empty or enemy then valid
+                addAvailPos(X+1, Y-1);
+            }
+        }
+    }
 }
